707863c8d0c8751deca6cea6bb0f3eb70db1fbc7
Victor Hidalgo <victorhidalgo@MacBook-Pro-de-Victor.local>
2026-06-21 20:47:25 -0500

    ux: move ambassador banner below benefits mosaic

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -11,8 +11,8 @@ export default function HomePage() {
     <>
       <LandingHero />
       <HowItWorksSection />
-      <AmbassadorInviteSection />
       <FeaturesGrid />
+      <AmbassadorInviteSection />
       <ExploreJuntasSection />
       <SocialProofSection />
       <FinalCTASection />
