0dcbec72fe4aac5b4a45746f6ecab9f54b8c8652
Victor Hidalgo <victorhidalgo@MacBook-Pro-de-Victor.local>
2026-06-21 21:14:00 -0500

    ux: refine ambassador home banner placement

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -13,8 +13,8 @@ export default function HomePage() {
       <HowItWorksSection />
       <FeaturesGrid />
       <ExploreJuntasSection />
-      <SocialProofSection />
       <AmbassadorHomeSection />
+      <SocialProofSection />
       <FinalCTASection />
     </>
   );
